edd8e797a9c0556d132f8f762227e872b0b731cc
Ismael <ismaelmtzballin@gmail.com>
2024-08-05 15:45:40 -0600

    feat: funcionalidad de api

--- Proyecto_Integrador/Api/app/routes/asistencia_alumnos.py
+++ Proyecto_Integrador/Api/app/routes/asistencia_alumnos.py
@@ -3,6 +3,45 @@ from sqlalchemy.orm import aliased
 from ..models import AsistenciaUsuarios, Usuario, Asistencia
 from .. import db
 from . import asistencia_alumnos_bp
+from datetime import datetime
+
+@asistencia_alumnos_bp.route('/guardar_uid', methods=['POST'])
+def guardar_uid():
+    data = request.form
+    uid = data.get('uid')
+    hora_actual = data.get('hora')
+    
+    usuario = Usuario.query.filter_by(uid=uid).first()
+    
+    if not usuario:
+        return jsonify({'message': 'Usuario no encontrado'}), 404
+    
+    # Obtener el registro de asistencia del día actual
+    fecha_actual = datetime.today().date()
+    asistencias = Asistencia.query.filter_by(fecha=fecha_actual).all()
+    
+    if not asistencias:
+        return jsonify({'message': 'No hay registro de asistencia para hoy'}), 404
+
+    # Convertir la hora actual a un objeto datetime.time
+    hora_actual_time = datetime.strptime(hora_actual, '%H:%M:%S').time()
+    
+    asistencia_valida = False
+
+    for asistencia in asistencias:
+        if asistencia.horaEntrada <= hora_actual_time <= asistencia.horaSalida:
+            asistencia_usuario = AsistenciaUsuarios.query.filter_by(id_Usuario=usuario.id, id_Asistencia=asistencia.id).first()
+            
+            if asistencia_usuario:
+                asistencia_usuario.asistencia = True
+                db.session.commit()
+                asistencia_valida = True
+                break
+
+    if asistencia_valida:
+        return jsonify({'message': 'Asistencia registrada correctamente'}), 200
+    else:
+        return jsonify({'message': 'La hora actual no está dentro del rango de entrada y salida'}), 400
 
 
 
@@ -46,3 +85,6 @@ def insert_asistencia():
     db.session.add(nueva_asistencia)
     db.session.commit()
     return jsonify({'message': 'Asistencia insertada correctamente'})
+
+
+
