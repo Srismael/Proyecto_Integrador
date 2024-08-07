c9667d4cf2167570f68a7ea55e07e27a7f24d992
Ismael <ismaelmtzballin@gmail.com>
2024-08-06 19:40:51 -0600

    feat: cambio realizado

--- Proyecto_Integrador/Api/app/routes/asistencia_alumnos.py
+++ Proyecto_Integrador/Api/app/routes/asistencia_alumnos.py
@@ -5,43 +5,22 @@ from .. import db
 from . import asistencia_alumnos_bp
 from datetime import datetime
 
-@asistencia_alumnos_bp.route('/guardar_uid', methods=['POST'])
-def guardar_uid():
-    data = request.form
-    uid = data.get('uid')
-    hora_actual = data.get('hora')
-    
-    usuario = Usuario.query.filter_by(uid=uid).first()
-    
-    if not usuario:
-        return jsonify({'message': 'Usuario no encontrado'}), 404
-    
-    # Obtener el registro de asistencia del día actual
-    fecha_actual = datetime.today().date()
-    asistencias = Asistencia.query.filter_by(fecha=fecha_actual).all()
-    
-    if not asistencias:
-        return jsonify({'message': 'No hay registro de asistencia para hoy'}), 404
 
-    # Convertir la hora actual a un objeto datetime.time
-    hora_actual_time = datetime.strptime(hora_actual, '%H:%M:%S').time()
-    
-    asistencia_valida = False
+@asistencia_alumnos_bp.route('/actualizar_asistencia', methods=['POST'])
+def actualizar_asistencia():
+    data = request.json
+    id_usuario = data['id_Usuario']
+    id_asistencia = data['id_Asistencia']
 
-    for asistencia in asistencias:
-        if asistencia.horaEntrada <= hora_actual_time <= asistencia.horaSalida:
-            asistencia_usuario = AsistenciaUsuarios.query.filter_by(id_Usuario=usuario.id, id_Asistencia=asistencia.id).first()
-            
-            if asistencia_usuario:
-                asistencia_usuario.asistencia = True
-                db.session.commit()
-                asistencia_valida = True
-                break
+    asistencia_usuario = AsistenciaUsuarios.query.filter_by(id_Usuario=id_usuario, id_Asistencia=id_asistencia).first()
 
-    if asistencia_valida:
-        return jsonify({'message': 'Asistencia registrada correctamente'}), 200
+    if asistencia_usuario:
+        asistencia_usuario.asistencia = True
+        db.session.commit()
+        return jsonify({'message': 'Asistencia actualizada correctamente'}), 200
     else:
-        return jsonify({'message': 'La hora actual no está dentro del rango de entrada y salida'}), 400
+        return jsonify({'message': 'Registro de asistencia no encontrado'}), 404
+
 
 
 
